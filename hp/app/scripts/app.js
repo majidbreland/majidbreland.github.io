a1c169c440e750f61b54040f3f659e8019b7ce6e
majidbreland <majid.breland@gmail.com>
2017-06-18 17:30:28 -0500

    app update:

---
+++ hp/app/scripts/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name halfpastApp
+ * @description
+ * # halfpastApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('halfpastApp', [
+        // 'ngAnimate',
+        // 'ngCookies',
+        // 'ngResource',
+        'ngRoute',
+        // 'ngSanitize',
+        // 'ngTouch'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/photo', {
+                templateUrl: 'views/photo.html',
+                controller: 'PhotoCtrl',
+                controllerAs: 'photo'
+            })
+            .when('/video', {
+                templateUrl: 'views/video.html',
+                controller: 'VideoCtrl',
+                controllerAs: 'video'
+            })
+            .when('/graphics', {
+                templateUrl: 'views/graphics.html',
+                controller: 'GraphicsCtrl',
+                controllerAs: 'graphics'
+            })
+            .otherwise({
+                redirectTo: '/'
+            })
+    });
+
---
+++ hp/app/scripts/controllers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name halfpastApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the halfpastApp
+ */
+angular.module('halfpastApp')
+    .controller('MainCtrl', ['$scope', function($scope) {
+    		
+            $scope.test1 = "test";
+        }]);
